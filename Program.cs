f2ca326a1b53dd6a8d881331f26bf22383ca093f
Aaron Miller <millerac333@gmail.com>
2018-08-27 11:31:00 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace expression_members
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
